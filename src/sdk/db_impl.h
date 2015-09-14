b343decccd67098569e53f855e748d30caa9f6c6
caijieming <caijieming@baidu.com>
2015-09-14 21:59:32 +0800

    #1: adjuest code style

--- src/sdk/db_impl.h
+++ src/sdk/db_impl.h
@@ -6,38 +6,40 @@
 #ifndef  __SRC/SDK/DB_IMPL_H_
 #define  __SRC/SDK/DB_IMPL_H_
 
+namespace mdt {
 // table in memory control structure
-struct tera_adapter {
+struct TeraAdapter {
     tera::Client* client;
-    std::map<std::string, tera::table*> tera_table_map; // <table_name, table desc>
+    std::map<std::string, tera::Table*> tera_table_map; // <table_name, table desc>
 };
 
-struct fs_adapter {
+struct FsAdapter {
     std::string root_path;
     // TODO:
 };
 
-class database_impl : public database {
-public:
-    // create fs namespace
-    static int CreateDB(std::string db_name, database** db);
-    // if db not exit, create it
-    static int OpenTable(const CreateRequest& request, CreateResponse* response, table** table_ptr);
-
-private:
-    std::string db_name;
-    std::map<std::string, table_impl*> table_map; // <table_name, table ptr>
-};
-
-class table_impl : public table {
+class TableImpl : public Table {
 public:
     int Put(const StoreRequest* request, StoreResponse* response, StoreCallback callback);
     int Put(const StoreRequest* request, StoreResponse* response);
 
 private:
     TableDescription table_desc;
-    tera_adapter _tera;
-    fs_adapter _fs;
+    TeraAdapter _tera;
+    FsAdapter _fs;
 };
 
+class DatabaseImpl : public Database {
+public:
+    // create fs namespace
+    static int CreateDB(std::string db_name, Database** db);
+    // if db not exit, create it
+    static int OpenTable(const CreateRequest& request, CreateResponse* response, Table** table_ptr);
+
+private:
+    std::string db_name;
+    std::map<std::string, TableImpl*> table_map; // <table_name, table ptr>
+};
+
+}
 #endif  //__SRC/SDK/DB_IMPL_H_
--- src/sdk/sdk.h
+++ src/sdk/sdk.h
@@ -25,7 +25,7 @@ struct Index {
 struct StoreRequest {
     std::string db_name;
     std::string table_name;
-    std::string primary_key;
+    std::string primary_key; // key after encode
     uint64_t timestamp;
     std::vector<struct Index> index_list;
     std::string data;
@@ -72,14 +72,19 @@ struct SearchRequest {
     std::string db_name;
     std::string table_name;
     std::vector<struct IndexCondition> index_condition_list;
-    struct IndexCondition start_tm;
-    struct IndexCondition end_tm;
+    struct IndexCondition start_timestamp;
+    struct IndexCondition end_timestamp;
     int32_t limit;
 };
 
 // 查询结果
-struct SearchResponse {
+struct ResultStream {
+    std::string primary_key;
     std::vector<std::string> result_data_list;
+};
+
+struct SearchResponse {
+    std::vector<ResultStream> result_stream;
     void* user_ptr;
 };
 
@@ -143,7 +148,7 @@ void Create(const CreateRequest& request, CreateResponse* response);
 class Database {
 public:
     static int CreateDB(std::string db_name);
-    virtual int OpenTable(const CreateRequest& request, CreateResponse* response, table** table_ptr) = 0;
+    virtual int OpenTable(const CreateRequest& request, CreateResponse* response, Table** table_ptr) = 0;
 private:
     Database(const Database&);
     void operator=(const Database&);
