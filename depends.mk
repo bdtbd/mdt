a67774d2b78cf048e4888074e7b2819a1598b6f7
00k <likangd@gmail.com>
2015-09-17 22:43:20 +0800

    fix Makefile & build script

--- depends.mk
+++ depends.mk
@@ -5,10 +5,55 @@
 #       automatically config this for you.
 ################################################################
 
-BOOST_INCDIR=../thirdparty/boost_1_57_0
+BOOST_INCDIR=thirdparty/boost_1_57_0
+SOFA_PBRPC_PREFIX=thirdparty
+PROTOBUF_PREFIX=thirdparty
+SNAPPY_PREFIX=thirdparty
+ZOOKEEPER_PREFIX=thirdparty
+GFLAGS_PREFIX=thirdparty
+GLOG_PREFIX=thirdparty
+GTEST_PREFIX=thirdparty
+GPERFTOOLS_PREFIX=thirdparty
+INS_PREFIX=thirdparty
+TERA_PREFIX=thirdparty
+
+SOFA_PBRPC_INCDIR = $(SOFA_PBRPC_PREFIX)/include
+PROTOBUF_INCDIR = $(PROTOBUF_PREFIX)/include
+SNAPPY_INCDIR = $(SNAPPY_PREFIX)/include
+ZOOKEEPER_INCDIR = $(ZOOKEEPER_PREFIX)/include
+GFLAGS_INCDIR = $(GFLAGS_PREFIX)/include
+GLOG_INCDIR = $(GLOG_PREFIX)/include
+GTEST_INCDIR = $(GTEST_PREFIX)/include
+GPERFTOOLS_INCDIR = $(GPERFTOOLS_PREFIX)/include
+INS_INCDIR = $(INS_PREFIX)/include
+TERA_INCDIR = $(TERA_PREFIX)/include
+
+SOFA_PBRPC_LIBDIR = $(SOFA_PBRPC_PREFIX)/lib
+PROTOBUF_LIBDIR = $(PROTOBUF_PREFIX)/lib
+SNAPPY_LIBDIR = $(SNAPPY_PREFIX)/lib
+ZOOKEEPER_LIBDIR = $(ZOOKEEPER_PREFIX)/lib
+GFLAGS_LIBDIR = $(GFLAGS_PREFIX)/lib
+GLOG_LIBDIR = $(GLOG_PREFIX)/lib
+GTEST_LIBDIR = $(GTEST_PREFIX)/lib
+GPERFTOOLS_LIBDIR = $(GPERFTOOLS_PREFIX)/lib
+INS_LIBDIR = $(INS_PREFIX)/lib
+TERA_LIBDIR = $(TERA_PREFIX)/lib
+
+PROTOC = $(PROTOBUF_PREFIX)/bin/protoc
 
 ################################################################
 # Note: No need to modify things below.
 ################################################################
 
-DEPS_INCPATH = -I$(BOOST_INCDIR)
+DEPS_INCPATH = -I$(SOFA_PBRPC_INCDIR) -I$(PROTOBUF_INCDIR) \
+               -I$(SNAPPY_INCDIR) -I$(ZOOKEEPER_INCDIR) \
+               -I$(GFLAGS_INCDIR) -I$(GLOG_INCDIR) -I$(GTEST_INCDIR) \
+               -I$(GPERFTOOLS_INCDIR) -I$(BOOST_INCDIR) -I$(INS_INCDIR) \
+               -I$(TERA_INCDIR)
+DEPS_LDPATH = -L$(TERA_LIBDIR) -L$(SOFA_PBRPC_LIBDIR) -L$(PROTOBUF_LIBDIR) \
+              -L$(SNAPPY_LIBDIR) -L$(ZOOKEEPER_LIBDIR) \
+              -L$(GFLAGS_LIBDIR) -L$(GLOG_LIBDIR) -L$(GTEST_LIBDIR) \
+              -L$(GPERFTOOLS_LIBDIR) -L$(INS_LIBDIR) \
+              -L$(TERA_LIBDIR)
+DEPS_LDFLAGS = -ltera -lins_sdk -lsofa-pbrpc -lprotobuf -lsnappy -lzookeeper_mt \
+               -lgtest_main -lgtest -lglog -lgflags -ltcmalloc_minimal
