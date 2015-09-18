71aef651109805216bb7d02dc95642e38a6c6029
00k <likangd@gmail.com>
2015-09-18 10:59:40 +0800

    update Get impl

--- src/sdk/table_impl.h
+++ src/sdk/table_impl.h
@@ -26,6 +26,7 @@ struct FileLocation {
 
 public:
     std::string SerializeToString() {return std::string("123");}
+    void ParseFromString(const std::string& str) {};
 };
 
 struct TeraAdapter {
@@ -55,6 +56,19 @@ struct FilesystemAdapter {
     DataWriter* writer_;
 };
 
+enum COMPARATOR_EXTEND {
+    kBetween = 100
+};
+
+struct IndexConditionExtend {
+    std::string index_name;
+    enum COMPARATOR comparator;
+    std::string compare_value1;
+    std::string compare_value2;
+    bool flag1;
+    bool flag2;
+};
+
 class TableImpl : public Table {
 public:
     TableImpl(const TableDescription& table_desc,
@@ -71,6 +85,18 @@ public:
     static int OpenTable(const std::string& db_name, const TeraOptions& tera_opt,
                          const FilesystemOptions& fs_opt, const TableDescription& table_desc,
                          Table** table_ptr);
+
+    Status ExtendIndexCondition(const std::vector<IndexCondition>& index_condition_list,
+                                std::vector<IndexConditionExtend>* index_condition_ex_list);
+
+    void GetPrimaryKeys(const std::vector<IndexConditionExtend>& index_condition_ex_list,
+                        int64_t start_timestamp, int64_t end_timestamp,
+                        std::vector<std::string>* primary_key_list);
+
+    Status GetRows(const std::vector<std::string>& primary_key_list,
+                   std::vector<ResultStream>* row_list);
+
+    Status GetSingleRow(const std::string& primary_key, ResultStream* result);
 
 private:
     DataWriter* GetDataWriter();
