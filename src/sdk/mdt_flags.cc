310a21495c1b8790f8545931a743c273cde018f1
caijieming <caijieming@baidu.com>
2015-09-25 15:00:35 +0800

    #29 : interface test

--- src/sdk/mdt_flags.cc
+++ src/sdk/mdt_flags.cc
@@ -1,8 +1,8 @@
 #include <gflags/gflags.h>
 
 // nfs cluster location
-DEFINE_string(env_fs_type, "nfs", "file system type");
-DEFINE_string(env_nfs_mountpoint, "/disk/tera/mdt-test/", "nfs mount point");
+DEFINE_string(env_fs_type, "dfs", "file system type");
+DEFINE_string(env_nfs_mountpoint, "/disk/tera", "nfs mount point");
 DEFINE_string(env_nfs_conf_path, "./conf/nfs.conf", "nfs's client configure file");
 DEFINE_string(env_nfs_so_path, "./lib/libnfs.so", "nfs's client's .so lib");
 
@@ -11,4 +11,4 @@ DEFINE_string(tera_flag_file_path, "./conf/tera.flag", "tera's client's configur
 DEFINE_string(tera_root_dir, "/disk/tera/trace-sys/", "tera cluster's dir");
 
 // mdt cluster
-DEFINE_string(database_root_dir, "/disk/tera/", "database's data file dir");
+DEFINE_string(database_root_dir, "/disk/tera/DatabaseDir", "database's data file dir");
