d26552c7c9d87c2c9982bcec3ca1e2e11db4ca70
caijieming <caijieming@baidu.com>
2015-11-06 11:07:14 +0800

    log-collector support, ZIPLOG impl

---
+++ application/zip/src/trace/logger.cc
@@ -0,0 +1,41 @@
+#include "trace/logger.h"
+#include "trace/trace.h"
+
+namespace mdt {
+void InitTraceModule(const std::string& flagfile) {
+    TraceModule::InitTraceModule(flagfile);
+}
+
+void OpenTrace(uint64_t trace_id = 0,
+               uint64_t parent_span_id = 0,
+               uint64_t span_id = 0,
+               std::string& name) {
+    Trace::OpenTrace(trace_id, parent_span_id, span_id, name);
+}
+
+void AttachTrace(uint64_t attach_id) {
+    Trace::AttachTrace(attach_id);
+}
+
+void DetachTrace(uint64_t attach_id) {
+    Trace::DetachTrace(attach_id);
+}
+
+void ReleaseTrace() {
+    Trace::ReleaseTrace();
+}
+
+TraceIdentify GetTraceIdentify() {
+    TraceIdentify id;
+    Trace* trace = Trace::TopThreadValue();
+    if (trace == NULL) {
+        return id;
+    }
+    id.CopyFrom(trace->GetTraceID());
+    return id;
+}
+
+void Log(int level, const char* fmt, ...);
+void Log(int level, const char* value, const char* fmt, ...);
+}
+
